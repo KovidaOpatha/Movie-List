00b3856522e2294e80eb267eb785641d68660947
Kovida Opatha <kovidaopathaz@gmail.com>
2025-06-27 22:53:32 +0530

    create navbar and setup routes

---
+++ frontend/src/components/NavBar.jsx
@@ -0,0 +1,13 @@
+function NavBar() {
+    return <nav className="navbar">
+        <div className="navbar-brand">
+            <Link to ="/"> Movie App </Link>
+        </div>
+        <div className="navbar-links">
+            <Link to ="/" className="nav-link">Home</Link>
+            <Link to ="/favourites" className="nav-link">Favourites</Link>
+        </div>
+    </nav>
+}
+
+export default NavBar;
---
+++ frontend/src/pages/Favourites.jsx
@@ -0,0 +1,10 @@
+function Favourites() {
+    return (
+        <div className="favourites-empty">
+            <h2>No Favourite Movies Yet</h2>
+            <p>Add some movies to your favourites list to see them here.</p>
+        </div>
+    )
+}
+
+export default Favourites;
