a57f8ed0cd8e7c54cd234b8a064c51d2f1fce2b6
Kovida Opatha <kovidaopathaz@gmail.com>
2025-06-27 20:27:48 +0530

    update basic search function with useState

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,6 +1,9 @@
-import MovieCard from "../components/MovieCard";   
+import MovieCard from "../components/MovieCard";
+import {useState} from "react";
 
 function Home() {
+    const [searchQuery, setSearchQuery] = useState("");
+
     const movies = [
         {id: 1, title: "Jhon Wick", release_date: "2019"},
         {id: 2, title: "Avengers", release_date: "2018"},
@@ -9,12 +12,27 @@ function Home() {
         {id: 5, title: "Avatar", release_date: "2021"},
     ]
 
+    const handleSearch = (e) => {
+        e.preventDefault();
+        alert(`Searching for: ${searchQuery}`);
+        setSearchQuery("Search for a movie...");
+    }
+
 
     return (
         <div className = "home">
+            <form onSubmit={handleSearch} className="search-form">
+                <input type="text" 
+                placeholder="Search for a movie..."
+                className="search-input" 
+                value={searchQuery}
+                onChange={(e) => setSearchQuery(e.target.value)}
+                />
+                <button type="submit" className="search-button">Search</button>
+            </form>
             <div className = "movie-grid">
                 {movies.map((movie) => (
-                    <MovieCard movie={movie} key={movie.id} />
+                    movie.title.toLowerCase().startsWith(searchQuery) && <MovieCard movie={movie} key={movie.id} />
                 ))}
             </div>
         </div>
