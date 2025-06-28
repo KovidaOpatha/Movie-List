7b0cd49a0ec65fbc6d64e97519fd4147694abcd3
Kovida Opatha <kovidaopathaz@gmail.com>
2025-06-28 14:30:47 +0530

    finalized project

--- frontend/src/components/MovieCard.jsx
+++ frontend/src/components/MovieCard.jsx
@@ -19,7 +19,7 @@ function MovieCard({movie}) {
             <img src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`} alt={movie.title} />
             <div className="movie-overlay">
                 <button className={`favorite-btn ${favorite ? "active" : ""}`} onClick={onFavoriteClick}>
-                    🤍
+                    {favorite ? "❤️" : "🤍"}
                 </button>
             </div>
         </div>
